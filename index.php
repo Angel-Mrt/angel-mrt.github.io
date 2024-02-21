bffc007a2464b5c4c7876e156e8aa0b4c61178bd
Angel-Mrt <142117432+Angel-Mrt@users.noreply.github.com>
2024-02-20 19:44:25 -0600

    Subida de archivos index

---
+++ index.php
@@ -0,0 +1,280 @@
+<?php
+require 'includes/funciones.php';
+incluirTemplate('header', $inicio = true);
+
+?>
+
+
+<main class="contenedor seccion">
+    <h1 id="sobre_mi">Sobre Mí</h1>
+
+    <p> Soy un Ingeniero en Sistemas y Desarrollador Web FullStack
+        con un año de experiencia en el desarrollo, mantenimiento y
+        despliegue de aplicaciones web utilizando una amplia gama de
+        tecnologías. Mis conocimientos se han obtenido tanto en la universidad
+        como de manera autodidacta a través de cursos, documentación y vídeos.
+        Me apasiona aprender constantemente y como Freelance, me enfrento a nuevos desafíos y herramientas cada día para satisfacer las necesidades que se me presentan.</p>
+    <h2 id="tecnologias">Tecnologias</h2>
+    <div class="iconos-nosotros">
+        <div class="iconos">
+            <img src="build/img/html.png" alt="Icono HTML" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/css.png" alt="Icono CSS" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/js.png" alt="Icono JS" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/php.png" alt="Icono PHP" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/java.png" alt="Icono JAVA" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/mysql.png" alt="Icono MySQL" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/mongo.png" alt="Icono Mongo DB" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/postman.png" alt="Icono Postman" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/BitBucket.png" alt="Icono BitBucket" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/node.svg" alt="Icono Node Js" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/boots.svg" alt="Icono Bootstrap" loading="lazy">
+        </div>
+        <div class="iconos">
+            <img src="build/img/typescript.svg" alt="Icono typescript" loading="lazy">
+        </div>
+
+    </div>
+</main>
+
+<section id="proyectos" class="seccion contenedor">
+    <h2>Proyectos</h2>
+
+    <div class="p-center">
+        <p>Proyectos en los que he trabajado</p>
+    </div>
+    <div class="contenedor-anuncios">
+        <div class="anuncio">
+            <picture>
+                <source srcset="build/img/maxi.webp" type="image/webp">
+                <source srcset="build/img/maxi.jpg" type="image/jpg">
+                <img loading="lazy" src="build/img/maxi.jpg" alt="Pagina Maxibandas">
+            </picture>
+
+            <div class="contenido-anuncio">
+                <h3>MaxiBandas</h3>
+                <ul class="iconos-caracteristicas">
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/html.png" alt="icono WC">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/css.png" alt="icono estacionamiento">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/js.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/php.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/mysql.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/boots.svg" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/node.svg" alt="icono_dormitorio">
+                    </li>
+                </ul>
+
+                <a href="https://www.maxibandas.mx/" class=" boton-amarillo-bl">
+                    Visitar
+                </a>
+            </div> <!--Contenido Anuncio-->
+        </div><!--Anuncio-->
+
+        <div class="anuncio">
+            <picture>
+                <source srcset="build/img/itschi.webp" type="image/webp">
+                <source srcset="build/img/itschi.jpg" type="image/jpg">
+                <img loading="lazy" src="build/img/itschi.jpg" alt="Pagina ITSCHI">
+            </picture>
+
+            <div class="contenido-anuncio">
+                <h3>ITSCHI</h3>
+                <ul class="iconos-caracteristicas">
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/html.png" alt="icono WC">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/css.png" alt="icono estacionamiento">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/js.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/php.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/mysql.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/boots.svg" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/node.svg" alt="icono_dormitorio">
+                    </li>
+                </ul>
+
+                <a href="https://itschicontepec.edu.mx/" class=" boton-amarillo-bl">
+                    Visitar
+                </a>
+            </div> <!--Contenido Anuncio-->
+        </div><!--Anuncio-->
+
+
+        <div class="anuncio">
+            <picture>
+                <source srcset="build/img/fest.webp" type="image/webp">
+                <source srcset="build/img/fest.jpg" type="image/jpg">
+                <img loading="lazy" src="build/img/fest.jpg" alt="Pagina Festival de Musica">
+            </picture>
+
+            <div class="contenido-anuncio">
+                <h3>Festival de Musica</h3>
+                <ul class="iconos-caracteristicas">
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/html.png" alt="icono WC">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/css.png" alt="icono estacionamiento">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/js.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/node.svg" alt="icono_dormitorio">
+                    </li>
+                </ul>
+
+                <a href="https://festivalmusica-dev.netlify.app/" class=" boton-amarillo-bl">
+                    Visitar
+                </a>
+            </div> <!--Contenido Anuncio-->
+        </div><!--Anuncio-->
+
+        <div class="anuncio">
+            <picture>
+                <source srcset="build/img/BlogCafe.webp" type="image/webp">
+                <source srcset="build/img/BlogCafe.jpg" type="image/jpg">
+                <img loading="lazy" src="build/img/BlogCafe.jpg" alt="BLog de Cafe">
+            </picture>
+
+            <div class="contenido-anuncio">
+                <h3>Blog De Cafe</h3>
+                <ul class="iconos-caracteristicas">
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/html.png" alt="icono WC">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/css.png" alt="icono estacionamiento">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/js.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/node.svg" alt="icono_dormitorio">
+                    </li>
+                </ul>
+
+                <a href="https://blogcafe-angelmrt.netlify.app/" class=" boton-amarillo-bl">
+                    Visitar
+                </a>
+            </div> <!--Contenido Anuncio-->
+        </div><!--Anuncio-->
+
+        <div class="anuncio">
+            <picture>
+                <source srcset="build/img/bienesraices.webp" type="image/webp">
+                <source srcset="build/img/bienesraices.jpg" type="image/jpg">
+                <img loading="lazy" src="build/img/bienesraices.jpg" alt="Pagina de Bienes Raices">
+            </picture>
+
+            <div class="contenido-anuncio">
+                <h3>Blog De Bienes Raices</h3>
+                <ul class="iconos-caracteristicas">
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/html.png" alt="icono WC">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/css.png" alt="icono estacionamiento">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/js.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/php.png" alt="icono_dormitorio">
+                    </li>
+                    <li>
+                        <img class="iconos" loading="lazy" src="build/img/node.svg" alt="icono_dormitorio">
+                    </li>
+                </ul>
+
+                <a href="https://bienesraicesmrt.netlify.app/" class=" boton-amarillo-bl">
+                    Visitar
+                </a>
+            </div> <!--Contenido Anuncio-->
+        </div><!--Anuncio-->
+
+
+
+    </div><!---Contenedor-anuncio-->
+    <!---<div class="alinear-derecha">
+        <button onclick="window.location.href='mailto:angelmrtnic@gmail.com';">Enviar mensaje</button>
+    </div>
+    -->
+</section>
+
+<section class="imagen-contacto">
+    <h2>Me gustaria Colaborar en mas proyectos</h2>
+    <p>Estoy en total disponibilidad para ayudarte a llevar tus ideas al mundo digital
+        y hacer que tu presencia en Internet sea única y efectiva</p>
+    <p>Para mas informacion puedes mandarme mensaje a mi WhatsApp:</p>
+    <button onclick="window.location.href='https://wa.me/7461170452';" class="boton-amarillo">Enviar mensaje</button>
+</section>
+
+<h2 id="contacto">Contacto</h2>
+
+<div class="iconos-contacto ">
+    <div class="iconos">
+        <a href="mailto:angelmrtnic@gmail.com">
+            <img src="build/img/gmail.png" alt="Gmail" loading="lazy">
+        </a>
+    </div>
+    <div class="iconos">
+        <a href="https://wa.me/7461170452">
+            <img src="build/img/whatsapp.png" alt="Whatsapp" loading="lazy">
+        </a>
+
+    </div>
+    <div class="iconos">
+        <a href="https://www.linkedin.com/in/fernando-angel-mart%C3%ADnez-nicol%C3%A1s-624497274/">
+            <img src="build/img/linkedin.png" alt="Linkedin" loading="lazy">
+        </a>
+    </div>
+
+</div>
+</div>
+
+<?php
+incluirTemplate('footer');
+?>
